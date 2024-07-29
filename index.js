2d3abb45f195176d064f54b31c9847459538bc13
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-07-29 09:28:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,70 @@
-const express = require("express"); // Import Express
-const path = require("path"); // Import path module
-const app = express(); // Create an Express app
-const port = process.env.PORT || 3000; // Set server port
+import express from "express";
+import { createServer } from "node:http";
+import { publicPath } from "ultraviolet-static";
+import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
+import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
+import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
+import { join } from "node:path";
+import { hostname } from "node:os";
+import wisp from "wisp-server-node"
 
-// Serve static files from the 'public' directory
-app.use(express.static("public"));
+const app = express();
+// Load our publicPath first and prioritize it over UV.
+app.use(express.static(publicPath));
+// Load vendor files last.
+// The vendor's uv.config.js won't conflict with our uv.config.js inside the publicPath directory.
+app.use("/uv/", express.static(uvPath));
+app.use("/epoxy/", express.static(epoxyPath));
+app.use("/baremux/", express.static(baremuxPath));
 
-// Route for '/' serving the index.html file
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-// Route for '/portal' serving the public\portal\index.html file
-app.get("/portal", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/portal", "index.html"));
-});
-
-// Route for '/byeblock' serving the Byeblocker.html file
-app.get("/byeblock", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "Byeblocker.html"));
-});
-
-// Route for '/fart' serving the Fart.html file
-app.get("/fart", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "Fart.html"));
-});
-
-// Route for '/list' serving the list.html file
-app.get("/list", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "list.html"));
-});
-
-// Route for '/game' serving the games.html file
-app.get("/games", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "games.html"));
-});
-
-// Handle 404 errors
+// Error for everything else
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
+  res.status(404);
+  res.sendFile(join(publicPath, "404.html"));
 });
 
-// Creating server to listen at localhost 3000
-app.listen(3000, function (req, res) {
-  // Logging when the server has started
-  console.log("listening to server 3000");
+const server = createServer();
+
+server.on("request", (req, res) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  app(req, res);
+});
+server.on("upgrade", (req, socket, head) => {
+  if (req.url.endsWith("/wisp/"))
+    wisp.routeRequest(req, socket, head);
+  else
+    socket.end();
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`app listening on port ${port} at localhost:${port}`);
+let port = parseInt(process.env.PORT || "");
+
+if (isNaN(port)) port = 8080;
+
+server.on("listening", () => {
+  const address = server.address();
+
+  // by default we are listening on 0.0.0.0 (every interface)
+  // we just need to list a few
+  console.log("Listening on:");
+  console.log(`\thttp://localhost:${address.port}`);
+  console.log(`\thttp://${hostname()}:${address.port}`);
+  console.log(
+    `\thttp://${address.family === "IPv6" ? `[${address.address}]` : address.address
+    }:${address.port}`
+  );
+});
+
+// https://expressjs.com/en/advanced/healthcheck-graceful-shutdown.html
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+function shutdown() {
+  console.log("SIGTERM signal received: closing HTTP server");
+  server.close();
+  process.exit(0);
+}
+
+server.listen({
+  port,
 });
