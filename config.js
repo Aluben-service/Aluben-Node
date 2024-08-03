4d4e467e581e4f0c490e1fd3dfa10055dbf79659
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-08-03 14:38:07 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
+// Credits to interstellar!
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
-  users: {
-    // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
-  },
+  keylogs: true
 };
 export default config;
