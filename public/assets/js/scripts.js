0ca76c4157f3ba0351068913987994e912412064
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-08-04 21:07:44 -0700

    Update scripts.js

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -2,8 +2,12 @@
 * Credits to 3kh0!
 **/
 
+let scripts = 5;
+let currentscript = 1;
+
 function script(text) {
-  console.log("%cScript Injection", "color: cyan; font-weight: 600; background: black; padding: 0 5px; border-radius: 5px", `Injected script ${text} (Credits to 3kh0)`);
+  console.log("%cScript Injection", "color: cyan; font-weight: 600; background: black; padding: 0 5px; border-radius: 5px", `Injected script ${currentscript}/${scripts} (${text}) (Credits to 3kh0)`);
+  currentscript += 1;
 }
 
 function isMobileDevice() {
@@ -16,32 +20,37 @@ analytics.setAttribute("async", "");
 analytics.setAttribute("defer", "");
 analytics.setAttribute("src", "https://scripts.simpleanalyticscdn.com/latest.js");
 document.head.append(analytics);
-script("1/4 (Simple Analytics)");
+script("Simple Analytics");
 
 const plausible = document.createElement("script");
 plausible.setAttribute("defer", "");
 plausible.setAttribute("data-domain", location.origin);
 plausible.setAttribute("src", "https://plausible.io/js/script.js");
 document.head.append(plausible);
-script("2/4 (Plausible Analytics)");
+script("Plausible Analytics");
 
 const main = document.createElement("script");
 main.setAttribute("src", "assets/js/script.js");
 document.head.append(main);
-script("3/4 (Main script)");
+script("Main script");
 
 const font = document.createElement("script");
 font.setAttribute("src", "https://kit.fontawesome.com/50c3ccf36f.js");
 font.setAttribute("crossorigin", "anonymous");
 document.head.append(font);
-script("4/4 (Font awesome)");
+script("Font awesome");
+
+const settings = document.createElement("script");
+settings.setAttribute("src", "main.js");
+document.head.append(settings);
+script("Settings stuff");
 
 if (isMobileDevice()) {
     const dialog = document.createElement("dialog");
     dialog.setAttribute("open", "");
-    dialog.setAttribute("id", "dialog");
+    dialog.setAttribute("id", "notice");
     dialog.innerHTML = '<p>This tool, Aluben is only meant for computers!</p>'
     document.body.append(dialog);
-    const closedDialog = document.querySelector("#dialog");
+    const closedDialog = document.querySelector("#notice");
     closedDialog.showModal();
 }
