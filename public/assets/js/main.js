5efaf61455660198bebc652ce1db2dd45a6d37bc
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-08-04 21:20:44 -0700

    Update main.js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -133,3 +133,21 @@ document.onkeydown = (e) => {
     }
   }
 };
+
+async function say() {
+  const response = await fetch("assets/json/say.json");
+  const says = await response.json();
+  const randomSplash = says[Math.floor(Math.random() * says.length)];
+
+  if (randomSplash === "%GAMES_NUMBER%") {
+    const games = await fetch(location.origin + "assets/json/games.json").json();
+    randomSplash = `There are ${games.length} games currently`;
+  } else if (randomSplash === "%SPLASH_NUMBER%") {
+    const splashCacheAll = await fetch("assets/json/say.json").json();
+    randomSplash = `There are ${splashCacheAll.length} of these messages!`;
+  }
+
+  document.querySelector("#splash").innerHTML = randomSplash;
+}
+
+say();
